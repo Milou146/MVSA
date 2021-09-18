2b35b0b96f75b8d70c581a30465af05f1a16df6e
BalackBellraD <57068690+BalackBellraD@users.noreply.github.com>
2021-09-18 18:37:21 +0200

    ajout de l'ammobox (peut être fixé)

--- entities/entities/ent_556_ammobox/shared.lua
+++ entities/entities/ent_556_ammobox/shared.lua
@@ -1,6 +1,6 @@
 ENT.Type = "anim"
 ENT.Base = "base_entity"
 
-ENT.Model = "models/items/ammo_556x45mm.mdl"
+ENT.Model = "models\items\ammobox_556.mdl"
 ENT.AmmoCount = 30
 ENT.AmmoName = "5.56×45mm NATO"
