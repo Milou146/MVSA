ec9921c2f917659416f72ab3fccc618cd382cf11
Milou146 <57072450+Milou146@users.noreply.github.com>
2021-09-25 18:45:05 +0200

    Update sh_config.lua

--- gamemode/sh_config.lua
+++ gamemode/sh_config.lua
@@ -432,7 +432,39 @@ AmmoList = {
 }
 
 NPCList = {
-    "npc_zombie"
+    "npc_vj_zss_zombie1",
+    "npc_vj_zss_zombie2",
+    "npc_vj_zss_zombie3",
+    "npc_vj_zss_zombie4",
+    "npc_vj_zss_zombie5",
+    "npc_vj_zss_zombie6",
+    "npc_vj_zss_zombie7",
+    "npc_vj_zss_zombie8",
+    "npc_vj_zss_zombie9",
+    "npc_vj_zss_zombie10",
+    "npc_vj_zss_zombie11",
+    "npc_vj_zss_zombie12",
+    "npc_vj_zss_zombfast1",
+    "npc_vj_zss_zombfast2",
+    "npc_vj_zss_zombfast3",
+    "npc_vj_zss_zombfast4",
+    "npc_vj_zss_zombfast5",
+    "npc_vj_zss_zombfast6",
+    "npc_vj_zss_cfastzombie",
+    "npc_vj_zss_burnzie",
+    "npc_vj_zss_cpzombie",
+    "npc_vj_zss_czombie",
+    "npc_vj_zss_czombietors",
+    "npc_vj_zss_draggy",
+    "npc_vj_zss_zhulk",
+    "npc_vj_zss_zboss",
+    "npc_vj_zss_zminiboss",
+    "npc_vj_zss_zombguard",
+    "npc_vj_zss_undeadstalker",
+    "npc_vj_zss_zp1",
+    "npc_vj_zss_zp2",
+    "npc_vj_zss_zp3",
+    "npc_vj_zss_zp4"
 }
 
 LootList = {
