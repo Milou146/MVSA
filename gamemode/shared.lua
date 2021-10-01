58bc9d59361646117faeaaa23037d41c14aeab4b
Milou146 <57072450+Milou146@users.noreply.github.com>
2021-10-01 20:05:19 +0200

    remove sandbox derivation

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,7 +2,6 @@ GM.Name = "MilitaryRP"
 GM.Author = "N/A"
 GM.Email = "N/A"
 GM.Website = "N/A"
-DeriveGamemode( "sandbox" )
 
 include( "player_class/player_usmc.lua" )
 include( "player_class/player_survivor.lua" )
